a564a684c5dfe227e0ee1a803f51a42b466284b5
Matt Glaman <nmd.matt@gmail.com>
2015-12-30 16:04:08 -0600

    Add README

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -11,21 +11,20 @@ cd "$DRUPAL_TI_DRUPAL_DIR"
 
 # Download custom branches of address and composer_manager.
 (
+    # These variables come from environments/drupal-*.sh
+    mkdir -p "$DRUPAL_TI_MODULES_PATH"
     cd "$DRUPAL_TI_MODULES_PATH"
+
     git clone --branch 8.x-1.x http://git.drupal.org/project/composer_manager.git
 )
 
 # Ensure the module is linked into the codebase.
-cd "$DRUPAL_TI_DRUPAL_DIR"
-MODULE_DIR=$(cd "$TRAVIS_BUILD_DIR"; pwd)
-
-mkdir -p "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
-
-# Point module into the drupal installation.
-cd "$DRUPAL_TI_DRUPAL_DIR"
-ln -sf "$MODULE_DIR" "$DRUPAL_TI_MODULES_PATH/$DRUPAL_TI_MODULE_NAME"
+drupal_ti_ensure_module_linked
 
 # Initialize composer_manager.
 php modules/composer_manager/scripts/init.php
 composer drupal-rebuild
 composer update -n --verbose
+
+# Enable main module and submodules.
+drush en -y geoip
